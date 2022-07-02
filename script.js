fb794a5b4b2231f279b3146ce86c3738b90ef127
sarahtang7 <sarahtang07@gmail.com>
2022-07-02 16:46:39 -0600

    Add JavaScript functions to add, subtract, multiply, and divide two numbers

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(n, m) {
+    return n + m;
+}
+
+function subtract(n, m) {
+    return n - m;
+}
+
+function multiply(n, m) {
+    return n * m;
+}
+
+function divide(n, m) {
+    return n / m;
+}
+
+console.log(add(5,2));
+console.log(subtract(5,2));
+console.log(multiply(5,2));
+console.log(divide(5,2));
